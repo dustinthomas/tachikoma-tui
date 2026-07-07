39948900ff26d9a6d0f8319f3949011e0bc4f115
Dustin <dhess@quantumcomputinginc.com>
2026-07-07 14:05:19 -0500

    chore: wire spc.jl into TachikomaTUI module + exports (enable merged visual tweaks)

--- src/TachikomaTUI.jl
+++ src/TachikomaTUI.jl
@@ -8,9 +8,14 @@ export Tachikoma
 
 # Include examples / apps
 include("hello.jl")
+include("spc.jl")
 
 # Public demo runners
 export HelloModel, hello_tachikoma, run_hello
+
+# SPC chart (visual tweaks: click vertical, markers, hover tooltip)
+export SPCData, SPCModel, Viewport, generate_spc_data
+export static_spc_demo, run_static_spc, spc_demo, run_spc
 
 # Add your own includes here as the project grows:
 # include("my_dashboard.jl")
